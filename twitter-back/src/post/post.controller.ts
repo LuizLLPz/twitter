9c21c4c75cc6b14752756166bf922cc937550720
LuizLLP <luizllp321@gmail.com>
2023-09-17 22:02:28 -0300

    feat: added services to post creation, fetching and deletion

--- twitter-back/src/post/post.controller.ts
+++ twitter-back/src/post/post.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import { response } from "express";
+import { Post as PostModel } from "@prisma/client";
+
 import { PostService } from './post.service';
 
 
@@ -7,7 +10,32 @@ export class PostController {
     constructor(private readonly postService: PostService) {}
 
     @Get('post')
-    getPosts() {
+    getPosts(): Promise<PostModel[]> {
         return this.postService.getPosts();
+    }
+
+    @Get('post?userId=:id')
+    getUserPosts(@Param('authorId') authorId?: number): Promise<PostModel[]> {
+        return this.postService.getUserPosts(authorId);
+    }
+
+    @Post('post')
+    savePost(@Body() post: PostModel): { message: string } {
+        try {
+            this.postService.savePost(post);
+            return { message: 'Post created successfully' };
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    @Delete('post/:id')
+    deletePost(@Param('id') id: number): { message: string } {
+        try {
+            this.postService.deletePost(id);
+            return { message: 'Post deleted successfully' };
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 }
--- twitter-back/src/post/post.service.ts
+++ twitter-back/src/post/post.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from "@prisma/client";
+
 import { PrismaService } from "../prisma.service";
+
 
 @Injectable()
 export class PostService {
     constructor(private prisma: PrismaService) {}
-    getPosts() {
-        return this.prisma.user.findMany();
+    getPosts(): Promise<Post[]> {
+        return this.prisma.post.findMany();
     }
+
+    getUserPosts(authorId: number): Promise<Post[]> {
+        return this.prisma.post.findMany({
+            where: {
+                authorId
+            }
+        })
+    }
+
+    savePost(data: Post): void {
+        this.prisma.post.create({
+            data
+        })
+    }
+
+    deletePost(id: number): void {
+        this.prisma.post.delete({
+            where: {
+                id
+            }
+        })
+    }
+
 }
