166ccb3cd33707ea0779651c0fce3a3fb9658e72
Martoooooo <martin.romao@hotmail.com>
2023-09-23 20:59:08 -0300

    Added login page; added route to login page;

--- twitter/src/app/app.module.ts
+++ twitter/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { MainComponent } from './modules/main/main.component';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
