0442ec73f4d0bd81cb35f476dfd1fd61add860e6
luizllpz <luizllp321@gmail.com>
2023-09-22 22:30:03 -0300

    refactor: remove controller padrão

--- twitter-back/src/app.module.ts
+++ twitter-back/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { LikeController } from "./post/like/like.controller";
+import { LikeService } from "./post/like/like.service";
 import { PerfilController } from './perfil/perfil.controller';
 import { PostController } from './post/post.controller';
-import { PrismaService } from './prisma.service';
 import { PostService } from './post/post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PostController, PerfilController],
-  providers: [AppService, PostService, PrismaService],
+  controllers: [PostController, LikeController, PerfilController],
+  providers: [PostService, LikeService, PrismaService],
 })
 export class AppModule {}
