0d1f92600f63bd403adc3979723722a5307eedb7
LuizLLP <luizllp321@gmail.com>
2023-09-23 21:57:44 -0300

    feat: merge feature post interactions into master

--- twitter-back/src/app.module.ts
+++ twitter-back/src/app.module.ts
@@ -6,12 +6,13 @@ import { PerfilController } from './perfil/perfil.controller';
 import { PostController } from './post/post.controller';
 import { PostService } from './post/post.service';
 import { PrismaService } from './prisma.service';
-import {CommentController} from "./post/comment/comment.controller";
-import {CommentService} from "./post/comment/comment.service";
+import { CommentController } from "./post/comment/comment.controller";
+import { CommentService } from "./post/comment/comment.service";
+import {PerfilService} from "./perfil/perfil.service";
 
 @Module({
   imports: [],
   controllers: [PostController, LikeController, PerfilController, CommentController],
-  providers: [PostService, LikeService, PrismaService, CommentService],
+  providers: [PrismaService, PostService, LikeService, PerfilService, CommentService],
 })
 export class AppModule {}
