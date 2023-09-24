06c6eaec1c7ea81e6ebbfb900463fef4ad02d552
LuizLLP <luizllp321@gmail.com>
2023-09-24 13:18:44 -0300

    feat: added https configuration for server

--- twitter-back/src/main.ts
+++ twitter-back/src/main.ts
@@ -1,10 +1,16 @@
+import * as fs from "fs";
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from "@nestjs/common";
 
 import { AppModule } from './app.module';
 
+const httpsOptions = {
+  key: fs.readFileSync('secrets/ca.key'),
+  cert: fs.readFileSync('secrets/ca.crt'),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
