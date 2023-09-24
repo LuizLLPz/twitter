5521f40c9ac3e3f40f5a72f20e2ebfae8199eb33
Martoooooo <martin.romao@hotmail.com>
2023-09-24 16:28:12 -0300

    Added fields for login

--- twitter/src/app/app.module.ts
+++ twitter/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { MainComponent } from './modules/main/main.component';
 import { LoginComponent } from './modules/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +23,14 @@ import { LoginComponent } from './modules/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
--- twitter/src/app/modules/login/login.component.ts
+++ twitter/src/app/modules/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  public form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.form= this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    })
+  }
 
 }
