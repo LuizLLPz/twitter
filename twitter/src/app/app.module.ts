0e56d53ec52821503db99d257793e11811943abb
mateus weinert <mateuswhj@gmail.com>
2023-09-23 21:53:00 -0300

    create-form

--- twitter/src/app/app.module.ts
+++ twitter/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { MainComponent } from './modules/main/main.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    MainComponent
+    MainComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ twitter/src/app/modules/register/_models/register-form.model.ts
@@ -0,0 +1,8 @@
+export interface RegisterForm{
+  name: string,
+  user: string,
+  email: string,
+  cellphone: number,
+  password: string,
+  confirm_password: string,
+}
