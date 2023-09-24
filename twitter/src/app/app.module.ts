097cf0a0c8c8110b70fe418ed696f0ce3ec9fcae
LuizLLP <luizllp321@gmail.com>
2023-09-24 12:59:00 -0300

    feat: added service interceptor for base api url

--- twitter/src/app/app.module.ts
+++ twitter/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ApiService } from "./api.service";
 import { AppComponent } from './app.component';
+import { BaseUrlInterceptor } from "./base-url.interceptor";
 import { RegisterComponent } from './modules/register/register.component';
 import { MainComponent } from './modules/main/main.component';
+import {FeedComponent} from "./modules/feed/feed.component";
+import { PostCardComponent } from './modules/feed/post-card/post-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FeedComponent,
     RegisterComponent,
-    MainComponent
+    MainComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BaseUrlInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ twitter/src/app/base-url.interceptor.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable()
+export class BaseUrlInterceptor implements HttpInterceptor {
+
+  constructor(private configService: ApiService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const baseUrl = this.configService.getBaseUrl();
+    const apiReq = request.clone({ url: `${baseUrl}/${request.url}` });
+    return next.handle(apiReq);
+  }
+}
